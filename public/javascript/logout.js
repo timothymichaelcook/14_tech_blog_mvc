055a490f452d5ee60bbe85868fd04b25586d2f8a
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-09 11:39:06 -0400

    Fixed error on line 7 in logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -4,7 +4,7 @@ async function logout() {
     headers: { 'Content-Type': 'application/json' }
   });
 
-  if (response) {
+  if (response.ok) {
     document.location.replace('/');
   } else {
     alert(response.statusText);
