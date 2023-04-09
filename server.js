858a45d6815ed5574634e05018641241af161432
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-09 14:35:24 -0400

    Finished server.js file, working on readme file

--- server.js
+++ server.js
@@ -25,3 +25,15 @@ app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
